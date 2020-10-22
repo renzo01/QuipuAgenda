128017b36e4b4cad088ca945090c13430d37ce51
renzo01 <renzogomez577@gmail.com>
2020-10-21 22:20:30 -0500

    Initial Commit

---
+++ QuipuAgenda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuipuAgenda
+//
+//  Created by Renzo Gomez on 10/21/20.
+//  Copyright © 2020 Equipo Dinamita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
