8635992ae5ba3449ecbc86544abf432bf797ff37
renzo01 <renzogomez577@gmail.com>
2020-11-07 16:09:54 -0500

    vista central de opciones

---
+++ QuipuAgenda/CenterController.swift
@@ -0,0 +1,32 @@
+//
+//  CenterController.swift
+//  QuipuAgenda
+//
+//  Created by Renzo Gomez on 11/7/20.
+//  Copyright © 2020 Equipo Dinamita. All rights reserved.
+//
+
+import UIKit
+
+class CenterController: UIViewController {
+
+    @IBOutlet weak var btnEditProfile: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
